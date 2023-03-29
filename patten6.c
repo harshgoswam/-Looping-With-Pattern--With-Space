55e09e2a740388b6cc3e1ee7c907d5afdef3c13c
harshgoswam <122149827+harshgoswam@users.noreply.github.com>
2023-03-29 17:06:44 +0530

    Add files via upload

---
+++ patten6.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+main()
+{
+	int c,r,s;
+	for(r=5;r>=1;r--)
+     {
+     	for(s=0;s<r;s++)
+     	{
+     		printf(" ");
+		 }
+       for(c=5;c>=r;c--)
+       {
+       		printf("%d",r);
+	   }
+	   printf("\n");
+	 }
+}
